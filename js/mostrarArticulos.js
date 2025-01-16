e99a4420969ad04913cf97db999c337fa6d0c2c1
Fred Tagle <fredtaglell@gmail.com>
2025-01-15 21:14:40 -0500

    correcion3

--- js/mostrarArticulos.js
+++ js/mostrarArticulos.js
@@ -8,7 +8,7 @@ export default function mostrarArticulos(name,description,precies,url,id) {
     articulo.innerHTML=`<div class="target__product"><img src=${url} alt="${name} imagen"></div>
                 <h3 class="target__name">${name}</h3>
                 <p class="target__description">${description}</p>
-                <p class="target__precies"><span>$ ${precies}</span><button id="${id}"  class="target__trash"><img src="./img/trash.svg"></button></p>`
+                <p class="target__precies"><span>$ ${precies}</span><button id="${id}"  class="target__trash"><img src="../img/trash.svg"></button></p>`
                 return articulo;
             }
             
